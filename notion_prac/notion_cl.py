2885f55be34672ec802c83f44fc35812155a8276
FrankWang <jiangda2@illinois.edu>
2022-09-17 00:19:42 -0500

    add experimental parser to notebook

--- notion_prac/notion_cl.py
+++ notion_prac/notion_cl.py
@@ -15,40 +15,49 @@ while NOTION_TOKEN == "":
     print("NOTION_TOKEN not found.")
     NOTION_TOKEN = input("Enter your integration token: ").strip()
 
-notion = Client(auth=NOTION_TOKEN)
-
 """
 ToDo:
-when try to determine which page to append blocks:
-1. query database using `notion.databases.query` with params:
-    **{"database_id": database_id
-    "filter":{
-        "property":"Name",
-        "title":{
-            "equals": page_name
+1. TheGitNotion Client interface
+    when try to determine which page to append blocks:
+    1. query database using `notion.databases.query` with params:
+        **{"database_id": database_id
+        "filter":{
+            "property":"Name",
+            "title":{
+                "equals": page_name
+            }
         }
-    }
-    }
-2. do some extraction to extract the page name
+        }
+    2. do some extraction to extract the page name
+    
+    3. using `notion.blocks.children.list` to get the children of page's blocks with param: page_id
+    
+    4. using recursion to get the children of `Problem to focus on page`
+    
+    5. add new focus problem using template 
 
-3. using `notion.blocks.children.list` to get the children of page's blocks with param: page_id
+2. The ContentParser interface
+    1. when title content include inline code, should parse into inline code format.
+    
+    2. when adding heading format or other format parse according corresponding dict template respectively.
+    
 
-4. using recursion to get the children of `Problem to focus on page`
 
-5. add new focus problem using template 
+3. other generic call to notion api should just use self.notion.xxx 
 """
 
 class MyGitNotion:
-    def __init__(self, page_id:Str=None, ) -> None:
-        self.database_id = os.getenv("database_id","")
-        self.page_id = page_id
-        self.database = None
-        self.blocks = None
+    def __init__(self, contentparser= NotionContentParser(), **kwargs) -> None:
+        self.notion = Client(auth=NOTION_TOKEN)
+        self.database_id = os.getenv("database_id", "")
+        self.database_pages = {}
+        self.contentparser = contentparser
+        # self.blocks = None
 
-    def get_page(self, name:Str) -> Dict:
-        self.database = notion.databases.query(
+    def get_page(self, name: str) -> Dict:
+        self.database_pages["name"] = self.notion.databases.query(
         **{
-            "database_id": os.getenv("database_id",""),
+            "database_id": self.database_id,
             "filter": {
                 "property": "Name",
                 "title":{
@@ -56,11 +65,26 @@ class MyGitNotion:
                     },
             },
         }) 
-        return self.database
+        return self.database_pages["name"]
 
-    def get_page_id(self, retrieved_page:Dict) -> Str:
-        self.page_id =  (retrieved_page['results'][0]['url']).split("-")[-1]
-        return self.page_id
-    
+    def get_page_id(self, name: str) -> str:
+        if name in self.database_pages.keys():
+            return (self.database_pages[name]["results"][0]["url"]).split("-")[-1]
+        else:
+            return (self.get_page(name)["results"][0]["url"]).split("-")[-1]
+
+    def get_page_children(self, name):
+        return self.notion.blocks.children(self.get_page_id(name))
+
+    def append_page_block(self, name, content, code=None):
+        parsed_res = self.contentparser.parse(content, code=code)
+        self.notion.blocks.append(self.get_page_id(name), children=parsed_res)
+
+
+class NotionContentParser:
+    pass
+
+
+
 
 
